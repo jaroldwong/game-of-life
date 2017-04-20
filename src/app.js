a4e9dec4cbf26927bcef66219d4c0e8a0967b5e8
Jarold Wong <jaroldw@gmail.com>
2017-04-20 16:24:26 -0700

    Toggle cells on grid

--- src/app.js
+++ src/app.js
@@ -3,14 +3,61 @@ var ReactDOM = require('react-dom');
 
 require('./style.css');
 
+// Game Variables
+var width = 50;
+var height = 30;
+
+
 var Board = React.createClass({
+    getInitialState: function() {
+        return {
+            gridWidth: width,
+            gridHeight: height,
+            game: new Array(width * height).fill(false)
+        }
+    },
     render: function() {
         return(
             <div>
-                Hello World!
+                <h1>Conway's Game of Life</h1>
+                <Grid cellArray={this.state.game} toggle={this.toggleCell}/>
             </div>
         );
+    },
+    toggleCell: function(index) {
+        var cellArray = this.state.game;
+        var currentState = cellArray[index];
+        cellArray[index] = !currentState;
+
+        this.setState(
+            {game: cellArray}
+        )
     }
 });
+
+var Grid = React.createClass({
+    render: function() {
+        var _this = this;
+        return(
+            <div className="grid">
+                {_this.props.cellArray.map(function(cellState, index) {
+                    if(cellState === false){
+                        return(
+                            <div className="cell" onClick={_this.handleClick.bind(_this, index)} key={index}></div>
+                         )
+                    } else {
+                        return (
+                            <div className="cell alive" onClick={_this.handleClick.bind(_this, index)} key={index}></div>
+                        )
+                    }
+            })}
+            </div>
+        )
+    },
+    handleClick: function(index) {
+        console.log(index)
+        this.props.toggle(index);
+    }
+})
 
 ReactDOM.render(<Board />, document.getElementById('root'));
