8946c1c1ccb03099fe2533d639aff1f938f0020b
Jarold Wong <jaroldw@gmail.com>
2017-04-21 11:22:58 -0700

    Count neighbor cells

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ var ReactDOM = require('react-dom');
 require('./style.css');
 
 // Game Variables
-var width = 50;
-var height = 30;
-
+var width = 10;
+var height = 5;
+var totalSquares = width * height;
 
 var Board = React.createClass({
     getInitialState: function() {
         return {
             gridWidth: width,
             gridHeight: height,
-            game: new Array(width * height).fill(false)
+            game: new Array(totalSquares).fill(false)
         }
     },
     render: function() {
@@ -40,8 +40,8 @@ var Grid = React.createClass({
         var _this = this;
         return(
             <div className="grid">
-                {_this.props.cellArray.map(function(cellState, index) {
-                    if(cellState === false){
+                {_this.props.cellArray.map(function(liveCell, index) {
+                    if(liveCell === false){
                         return(
                             <div className="cell" onClick={_this.handleClick.bind(_this, index)} key={index}></div>
                          )
@@ -55,8 +55,39 @@ var Grid = React.createClass({
         )
     },
     handleClick: function(index) {
-        console.log(index)
         this.props.toggle(index);
+        this.countLiveNeighbors(index);
+    },
+    getNeighbors: function(index) {
+        var offset = width + 1;
+        var offsetArray;
+
+        if (index % width === 0) {
+            // left edge
+            offsetArray = [-offset+1, -offset+2, 1, offset-1, offset]
+        } else if ((index+1) % width === 0){
+            // right edge
+            offsetArray = [-offset, -offset+1, -1, offset-2, offset-1]
+        } else {
+            offsetArray = [-offset, -offset+1, -offset+2, -1, 1, offset-2, offset-1, offset]
+        }
+
+        var inbound = function(num){                                                                  
+            return num >= 0 && num < totalSquares
+        }
+        var neighbors = offsetArray.map(function(offset) {return index + offset}).filter(inbound);
+        
+        return neighbors;
+    },
+    countLiveNeighbors: function(index) {
+        var neighbors = this.getNeighbors(index);
+        var that = this;
+        var neighborsStatus = neighbors.map(function(n) {return that.props.cellArray[n]});
+        var liveNeighbors = neighborsStatus.reduce(function(a, b) {
+            return a + b;
+        }, 0);
+        
+        return liveNeighbors;
     }
 })
 
